7a3c6ca1b45f8b6e54a98d7d69b834361cbbaf95
kill3rrrr <sthaakash.1995@gmail.com>
2017-03-17 12:26:07 +0545

    data types

---
+++ basic-types.py
@@ -0,0 +1,9 @@
+an_integer = 6
+a_float_point = 3.6
+a_boolean = True
+a_string = "askldj"
+
+print("integer value =", an_integer)
+print("floating point = ", a_float_point)
+print("a boolean = ", a_boolean)
+print("a string = ", a_string)
