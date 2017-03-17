89832ac40fe9da0b7643d502c4ef3520714175bd
kill3rrrr <sthaakash.1995@gmail.com>
2017-03-17 13:02:57 +0545

    first edit

--- quadratic.py
+++ quadratic.py
@@ -1,6 +1,7 @@
 a = 6
 b = 6
 c = 6
-x1 = (- b + (b ** 2 - 4 * a * c)) / (2 * a)
-x2 = (- b - (b ** 2 - 4 * a * c)) / (2 * a)
+d=(b ** 2 - 4 * a * c)
+x1 = (- b + d) / (2 * a)
+x2 = (- b - d) / (2 * a)
 print("the two values of x are ", x1, x2)
