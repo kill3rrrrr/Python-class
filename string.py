4c5bb6a1b1da71586ea73cd6b211f78afcd79533
kill3rrrr <sthaakash.1995@gmail.com>
2017-03-18 08:47:16 +0545

    amde some modifications to the strings

---
+++ string.py
@@ -0,0 +1,6 @@
+s = input('enter a string: ')
+
+print('you have entered '+ s)
+print('No of characters = %d' % len(s))
+print('first character of the string: %s' % s[0])
+print('last character of the string: %s' % s[len(s)-1])
