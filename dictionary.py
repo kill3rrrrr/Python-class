965ec83470570adb4db1b3ab5df280e29c767606
kill3rrrr <sthaakash.1995@gmail.com>
2017-03-19 13:42:37 +0545

    dictionary list

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+user_info = {
+	'name' : 'aakash shrestha',
+	'email' : 'sthaakash.1995@gmail.com',
+	'address' : 'kalanki'
+}
+print('name : %s' % user_info['name'])
+print('email : %s' % user_info['email'])
+print('address : %s' % user_info['address'])
